95ddc15699fc05ef299a3dc990b5410c4ec965db
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-26 16:11:38 +0100

    Deleted: commands/kube-inject.sh

--- commands/kube-inject.sh
+++
@@ -1,8 +0,0 @@
-istioctl kube-inject -f samples/sleep/sleep.yaml | kubectl apply -f -
-
-istioctl kube-inject \
-    --injectConfigFile inject-config.yaml \
-    --meshConfigFile mesh-config.yaml \
-    --valuesFile inject-values.yaml \
-    --filename samples/sleep/sleep.yaml \
-    | kubectl apply -f -
