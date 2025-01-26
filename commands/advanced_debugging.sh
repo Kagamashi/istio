a6200e1b649bd3282690d0d4d019ad1ec654dbcd
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-26 16:11:37 +0100

    Deleted: commands/advanced_debugging.sh

--- commands/advanced_debugging.sh
+++
@@ -1,7 +0,0 @@
-# Gathers information about the current state of the Istio service mesh, including configuration, 
-# logs, and cluster state, to create a comprehensive bug report. 
-# This is especially helpful when seeking support or reporting bugs.
-istioctl bug-report
-
-# Provides access to metrics for Istio components, services, and proxies. This command is useful for debugging performance and traffic issues.
-istioctl x metrics pods <pod-name>
