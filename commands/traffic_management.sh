5d477e0cbfff51c7a62bf6e68dec15a9d6020f4a
Mikolaj <mikolaj.borecki1@gmail.com>
2025-01-26 16:11:39 +0100

    Deleted: commands/traffic_management.sh

--- commands/traffic_management.sh
+++
@@ -1,7 +0,0 @@
-# Provides a detailed description of how traffic is being managed for a specific service, 
-# including applied policies like VirtualServices, DestinationRules, and mTLS settings.
-istioctl x describe service my-service -n default
-
-# Shows how traffic is being routed for a given host or service, 
-# detailing the associated VirtualService and routing rules.
-istioctl x route service my-service
