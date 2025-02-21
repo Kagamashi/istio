a9e22682280d02f71eb2e10bb2288dbd6abffb88
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-21 10:04:04 +0100

    Updated submodule references

--- 02_installation_and_upgrades/installation/istioctl_install.sh
+++ 02_installation_and_upgrades/installation/istioctl_install.sh
@@ -8,7 +8,5 @@ istioctl install --set components.egressGateways.enabled=true
 # To install Istio with a minimal profile:
 istioctl install --set profile=minimal # || demo || default
 
-
-
 # run to ensure that Istio was installed correctly
 istioctl verify-install
